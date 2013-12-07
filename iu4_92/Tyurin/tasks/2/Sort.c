e49a4e3063afed75e14c3f2d55c4468b20660b13
Maxim <ChernovMaxim@mail.ru>
2013-12-07 00:54:43 -0800

    Create Sort.c

---
+++ iu4_92/Tyurin/tasks/2/Sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main(int argc ,char** argv)
+{
+ int mas[argc];
+  for(int i=1; i<argc; i++)
+  {
+	mas[i]=atol(*(argv+i));
+}
+ int i1,j,bufer;
+  for(i1=0; i1<argc; i1++)
+   for(j=argc-2; j>=i1; j--)
+    if (mas[j]>mas[j+1])
+     {
+      bufer=mas[j]; mas[j]=mas[j+1];mas[j+1]=bufer; 
+     }
+   //printf("\n Sorted Array:\n");
+   for(i1=1; i1<argc; i1++)
+   printf("%d\n",mas[i1]); 
+	return 0;
+}
