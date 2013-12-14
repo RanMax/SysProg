ab208d5142568b7bcdc776f3c02795d08db20f2e
kirill934 <kirill934@mail.ru>
2013-12-14 12:29:37 +0300

    Update HelloWorld.c

--- iu4_93/Chistova/tasks/1/HelloWorld.c
+++ iu4_93/Chistova/tasks/1/HelloWorld.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
-//int max(int,int); //прототип функции
+
 void main()
 {
-	int x,y,z;
-	printf("Enter x,y(огрр)\n");
-	scanf("%d%d",&x,&y);
-	z=max(x,y);
 	printf("Hello world\n");
-	printf("x=%d y=%d z=%d\n",x,y,z);
-}
-int max(int a, int b)
-{
-	if (a>b)
-		return a;
-	else 
-		return b;
 }
