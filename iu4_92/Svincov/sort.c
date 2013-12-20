3f30c82c0690ffe98078985ed33d4203aa8567f0
Legrane <nikita_inferno@mail.ru>
2013-12-20 17:10:02 +0400

    Update and rename NewFile to sort.c

---
+++ iu4_92/Svincov/sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+
+    int n, i, j;
+
+    int a[argc-1];
+
+    for(i = 0; i < argc-1; i++) {
+
+        a[i]=atoi(argv[i+1]);
+
+    }
+
+    for(i = 0; i < argc-1; i++) {
+
+       // сравниваем два соседних элемента.
+
+       for(j = 0 ; j < argc - i - 2 ; j++) {
+
+           if(a[j] > a[j+1]) {
+
+              // если они идут в неправильном порядке, то
+
+              //  меняем их местами.
+
+              int tmp = a[j]; a[j] = a[j+1] ; a[j+1] = tmp;
+
+           }
+
+       }
+
+    }
+
+    printf("\n");
+
+    for (i=0; i < argc-1; i++){
+
+        printf("%d",a[i]);
+
+        printf("\n");
+
+    }
+
+ }
+
+
+
