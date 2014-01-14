b88b820066ee7fda6a313154f1a85b8aebf56837
xDDD1maNNN <pletnev1992@rambler.ru>
2014-01-14 04:15:29 -0800

    Create Create Sort.c

---
+++ iu4_93/Pletnev/tasks/2/Create Sort.c
@@ -0,0 +1,34 @@
++#include <stdio.h>
+ +#include <stdlib.h>
+ +void bubble_sort(int* data, int size);
+ +int main(int argc, char** argv)
+ +{
+ +    int mas[argc];
+ +    int i;
+ +        for(i=1; i<argc; i++)
+ +    {
+ +        mas[i-1]=atol(*(argv+i));
+ +    }
+ +    bubble_sort(mas, argc-1);
+ +    for(i=0; i<argc-1; i++)
+ +    {
+ +        printf("%d\n",mas[i]);
+ +    }
+ +    return 0;
+ +}
+ +void bubble_sort(int* data, int size)
+ +{
+ +        int i,j;
+ +        for (i=0; i<size; ++i)
+ +        {
+ +                for (j=size -1; j>i; --j)
+ +                {
+ +                        if (data[j]<data[j-1])
+ +                        {
+ +                                int t=data[j-1];
+ +                                data[j-1]=data[j];
+ +                                data[j]=t;
+ +                        }
+ +                }
+ +        }
+ +}
