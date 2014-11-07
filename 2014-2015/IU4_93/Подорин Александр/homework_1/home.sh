dfc2943af64ec0b8a80283a79cdfcdb175edeca1
GooG2e <alex@podorin.ru>
2014-11-07 23:45:29 +0300

    homework variant 1

---
+++ 2014-2015/IU4_93/Подорин Александр/homework_1/home.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+n_elem=$#
+if [ $n_elem -lt 3 ];
+then
+	echo "Not enough arguments: $n_elem"
+	exit 1
+else
+	move_dir=$1
+	log_file=$2
+	time_dir=`date +%Y-%m-%d%_H:%M`
+	move_dir="$move_dir/$time_dir"
+	if [ ! -d "$move_dir" ];
+	then
+		mkdir "$move_dir"
+	fi
+	until [ -z "$3" ]
+	do
+		cur_dir=$3
+		for file in `find $cur_dir -type f`
+		do
+			if [ -f file ];
+			then
+				echo "File Exists"
+				echo -e "$(date +%Y-%m-%d%_H:%M:%S) $file ERROR:Совпадение имени файла \n\r" >> $log_file
+			else
+				cp "$file" "$move_dir"
+			fi
+		done
+		shift
+	done
+fi
+echo "Copy finished"
+exit 0
