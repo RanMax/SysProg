c2e85b143f59e182c8126e7b5551aa1ad0ba1799
TOLS <votonia@gmail.com>
2014-11-10 21:55:50 +0300

    Update cronmyscript.sh

--- 2014-2015/RT2_91/Врублевский Александр/cronmyscript.sh
+++ 2014-2015/RT2_91/Врублевский Александр/cronmyscript.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Directory size script
 
+dir1="$1"
+dir2="$2"
+calc=$(du -sh "$dir1") 
 T=$(date +%H:%M:%S)
-D=$(date +%d.%m.%Y)
-echo -n "$D" "$T" " |  "
-du -hs /home/tols
+D=$(date +%Y-%m-%d)
+echo -n "$D" "$T" " |  " >> "$dir2"
+echo "$calc" >> "$dir2"
 exit 0
